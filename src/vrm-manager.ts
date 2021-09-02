9ac33a862f3ad274189b44ef7e6c9bbe3bb21b27
AzureRain <azrain@phantomsoftware.com>
2021-09-03 01:28:42 +0300

    Experimental physics

--- src/vrm-manager.ts
+++ src/vrm-manager.ts
@@ -1,4 +1,4 @@
-import { Vector3 } from '@babylonjs/core/Maths/math';
+import { Vector3, Matrix, Quaternion } from '@babylonjs/core/Maths/math';
 import { Mesh } from '@babylonjs/core/Meshes/mesh';
 import { TransformNode } from '@babylonjs/core/Meshes/transformNode';
 import { MorphTarget } from '@babylonjs/core/Morph/morphTarget';
@@ -7,9 +7,13 @@ import {SpringBoneController} from './secondary-animation/spring-bone-controller
 import { HumanoidBone } from './humanoid-bone';
 import { IVRM } from './vrm-interfaces';
 import {
+    AbstractMesh,
     Node,
-    Scene
+    PhysicsImpostor,
+    PhysicsImpostorParameters,
+    QuadraticErrorSimplification, Scene, Skeleton
 } from "@babylonjs/core";
+import { PhysicsViewer } from "@babylonjs/core/Debug";
 
 interface MorphTargetSetting {
     target: MorphTarget;
@@ -32,10 +36,43 @@ interface MeshCache {
     [meshIndex: number]: Mesh[];
 }
 
+interface NodeMeshInfo {
+    nodeIndex: number;
+    nodeMeshes: Mesh[];
+    nodeParent: Nullable<Node>;
+    nodeSkeleton: Nullable<Skeleton>;
+    nodeType: string;
+}
+
+interface MeshCacheInfo {
+    Body?: NodeMeshInfo;
+    Face?: NodeMeshInfo;
+    Hair?: NodeMeshInfo;
+}
+
+
 /**
  * Unity Humanoid Bone 名
  */
 export type HumanBoneName = 'hips' | 'leftUpperLeg' | 'rightUpperLeg' | 'leftLowerLeg' | 'rightLowerLeg' | 'leftFoot' | 'rightFoot' | 'spine' | 'chest' | 'neck' | 'head' | 'leftShoulder' | 'rightShoulder' | 'leftUpperArm' | 'rightUpperArm' | 'leftLowerArm' | 'rightLowerArm' | 'leftHand' | 'rightHand' | 'leftToes' | 'rightToes' | 'leftEye' | 'rightEye' | 'jaw' | 'leftThumbProximal' | 'leftThumbIntermediate' | 'leftThumbDistal' | 'leftIndexProximal' | 'leftIndexIntermediate' | 'leftIndexDistal' | 'leftMiddleProximal' | 'leftMiddleIntermediate' | 'leftMiddleDistal' | 'leftRingProximal' | 'leftRingIntermediate' | 'leftRingDistal' | 'leftLittleProximal' | 'leftLittleIntermediate' | 'leftLittleDistal' | 'rightThumbProximal' | 'rightThumbIntermediate' | 'rightThumbDistal' | 'rightIndexProximal' | 'rightIndexIntermediate' | 'rightIndexDistal' | 'rightMiddleProximal' | 'rightMiddleIntermediate' | 'rightMiddleDistal' | 'rightRingProximal' | 'rightRingIntermediate' | 'rightRingDistal' | 'rightLittleProximal' | 'rightLittleIntermediate' | 'rightLittleDistal' | 'upperChest' | string;
+
+
+const options: PhysicsImpostorParameters = {
+    mass: 0,
+    friction: 0.1,
+    restitution: 0.9,
+}
+const options_soft: PhysicsImpostorParameters = {
+    mass: 0.1,
+    friction: 0.2,
+    restitution: 0.9,
+    pressure: 0,
+    velocityIterations: 2,
+    positionIterations: 5,
+    stiffness: 1,
+    margin: 0.1,
+    damping: 0.05,
+}
 
 /**
  * VRM キャラクターを動作させるためのマネージャ
@@ -46,9 +83,18 @@ export class VRMManager {
     private transformNodeMap: TransformNodeMap = {};
     private transformNodeCache: TransformNodeCache = {};
     private meshCache: MeshCache = {};
+    private meshCacheInfo: MeshCacheInfo;
+    private physicsViewer: PhysicsViewer;
+    private physicsDebug: boolean;
     private _rootSkeleton: Node;
     private _humanoidBone: HumanoidBone;
     private _rootMesh!: Mesh;
+
+    // Whether hair meshes finished loading
+    private _hairReady: boolean = false;
+    public get hairReady(): boolean {
+        return this._hairReady;
+    }
 
     /**
      * Secondary Animation として定義されている VRM Spring Bone のコントローラ
@@ -371,5 +417,291 @@ export class VRMManager {
             }
         }
         return cache;
+    }
+
+    /**
+     * Add physics imposters to mesh primitives if physics engine
+     * is enabled in current scene.
+     * Must be the last step because of mesh merge.
+     */
+    public enablePhysics(debug=false) {
+        if (!this.scene.physicsEnabled) return;
+
+        this.physicsViewer = new PhysicsViewer(this.scene);
+        this.physicsDebug = debug;
+
+        // Body
+        this.addPhysicsImposters(this.meshCacheInfo.Body);
+
+        // Hair
+        this.addPhysicsImposters(this.meshCacheInfo.Hair);
+    }
+
+    private addPhysicsImposters(nodeMeshInfo: any) {
+        console.log(nodeMeshInfo.nodeParent);
+        const nodeIndex = nodeMeshInfo.nodeIndex;
+        if (nodeMeshInfo.nodeMeshes.length && nodeMeshInfo.nodeParent) {
+            switch (nodeMeshInfo.nodeType) {
+                case "Body":
+                    const imposterType = PhysicsImpostor.MeshImpostor;
+                    for (let i = 0; i < this.meshCache[nodeIndex].length; i++) {
+                        const thisMesh = this.meshCache[nodeIndex][i];
+                        // thisMesh.skeleton = null;
+                        let showImpostor = true;
+                        if (i == 0)
+                            showImpostor = true;
+
+                        this.reCalculateBoundingBox(thisMesh);
+                        this.createImposter(thisMesh, imposterType,
+                            options, true, true, false, showImpostor);
+                    }
+
+                    // MeshImposter cannot be parented
+                    // Using locked joints instead
+                    const mainBodyImpostor = this.meshCache[nodeIndex][0].physicsImpostor;
+                    for (let i = 0; i < this.meshCache[nodeIndex].length; i++) {
+                        if (i > 0 && i < 4 && mainBodyImpostor) {
+                            const mainImpostor = this.meshCache[nodeIndex][i].physicsImpostor;
+                            mainImpostor?.addJoint(
+                                mainBodyImpostor,
+                                new PhysicsJoint(
+                                    PhysicsJoint.LockJoint,
+                                    {
+                                        collision: false,
+                                    }
+                                )
+                            );
+                        }
+                    }
+
+                    // this.createImposter(nodeMeshInfo.nodeParent as Mesh, imposterType,
+                    //     options, true, false, false, false)
+                    break;
+                case "Hair":
+                    const mergedMesh = Mesh.MergeMeshes(nodeMeshInfo.nodeMeshes, true, true,
+                        undefined, false, true);
+                    this.logEssenstial(mergedMesh);
+
+                    if (mergedMesh) {
+                        // mergedMesh.skeleton = nodeMeshInfo.nodeSkeleton;
+                        mergedMesh.forceSharedVertices();
+
+                        const decimator = new QuadraticErrorSimplification(mergedMesh);
+                        decimator.simplify(
+                            {
+                                quality: 0.6,
+                                distance: 1,
+                                optimizeMesh: true
+                            },
+                            (mesh) => {
+                                if (mesh) {
+                                    this.setParentWrapper(mesh, nodeMeshInfo.nodeParent);
+                                    mesh.isVisible = true;
+                                    // mesh.forceSharedVertices();
+                                    mesh.skeleton = nodeMeshInfo.nodeSkeleton;
+                                    this.meshCache[nodeIndex].push(mesh);
+                                    mergedMesh.dispose();
+                                } else {
+                                    mergedMesh.setParent(nodeMeshInfo.nodeParent);
+                                    this.meshCache[nodeIndex].push(mergedMesh);
+                                }
+
+                                const thisMesh = this.meshCache[nodeIndex][0];
+                                this.createImposter(this.meshCache[nodeIndex][0], PhysicsImpostor.SoftbodyImpostor,
+                                    options_soft, true,false);
+
+
+                                if (this.meshCacheInfo.Body?.nodeParent) {
+                                    this.meshCacheInfo.Body.nodeParent.computeWorldMatrix(true);
+                                    const rigidImposter = (this.meshCacheInfo.Body.nodeParent as Mesh).physicsImpostor;
+                                    if (rigidImposter)
+                                        this.meshCache[nodeIndex][0].physicsImpostor?.addAnchor(
+                                            rigidImposter,
+                                            0.5, 0.3,
+                                            1.,
+                                            false
+                                        );
+                                }
+
+                                // Flag
+                                this._hairReady = true;
+                            }
+                        )
+
+                        while (this.meshCache[nodeIndex].length) {
+                            this.meshCache[nodeIndex].pop();
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Ignore relative transformations from parenting
+     * @param mesh Mesh to change parent
+     * @param parent Parent to change to
+     * @private
+     */
+    private setParentWrapper(mesh: Mesh, parent: Nullable<Node>) {
+        const prePosition = mesh.position.clone();
+        const preRotation = mesh.rotation.clone();
+        const preScaling = mesh.scaling.clone();
+        mesh.setParent(parent);
+        mesh.position = prePosition;
+        mesh.rotation = preRotation;
+        mesh.scaling = preScaling;
+    }
+
+    /**
+     * Recalculate bounding boxes
+     */
+    private reCalculateBoundingBox(mesh: Mesh) {
+        // Use facet instead of vertices since they can contain
+        mesh.updateFacetData();
+        console.log(mesh.facetNb);
+        const numeric_bound = 1e32;
+        const minVector = new Vector3(numeric_bound, numeric_bound, numeric_bound);
+        const maxVector = new Vector3(-numeric_bound, -numeric_bound, -numeric_bound);
+        let x, y, z;
+        for (let i=0; i<mesh.facetNb; i++) {
+            const facetPos = mesh.getFacetPosition(i);
+            x = facetPos.x;
+            y = facetPos.y
+            z = facetPos.z;
+            if (minVector.x > x)
+                minVector.x = x;
+            if (minVector.y > y)
+                minVector.y = y;
+            if (minVector.z > z)
+                minVector.z = z;
+            if (maxVector.x < x)
+                maxVector.x = x;
+            if (maxVector.y < y)
+                maxVector.y = y;
+            if (maxVector.z < z)
+                maxVector.z = z;
+        }
+
+        const worldMatrix = mesh.getWorldMatrix();
+        const transformedMinVector = Vector3.TransformCoordinates(minVector, worldMatrix);
+        const transformedMaxVector = Vector3.TransformCoordinates(maxVector, worldMatrix);
+
+        const newCenter = maxVector.add(minVector).multiply(new Vector3(0.5, 0.5, 0.5));
+        const transformMatrix = Matrix.Compose(
+            new Vector3(1, 1, 1), Quaternion.Identity(), newCenter.negate());
+
+        // // Add a small space over vertices
+        // const eps = 0.5;
+        // minVector = minVector.subtractFromFloats(eps, eps, eps);
+        // maxVector = maxVector.addInPlaceFromFloats(eps, eps, eps);
+
+        const boundingBox = mesh.getBoundingInfo().boundingBox;
+        console.log(minVector);
+        console.log(maxVector);
+        console.log(transformedMinVector);
+        console.log(transformedMaxVector);
+        console.log(Vector3.TransformCoordinates(boundingBox.center, worldMatrix));
+        mesh.setPivotPoint(newCenter);
+        mesh.getBoundingInfo().reConstruct(minVector, maxVector);
+
+        // mesh.bakeTransformIntoVertices(transformMatrix);
+        // mesh.position = newCenter;
+        // mesh.getBoundingInfo().reConstruct(
+        //     minVector.subtract(newCenter), maxVector.subtract(newCenter));
+    }
+
+    private gatherMeshInfo() {
+        const pattern = /^(Body|Face|Hair)\d*_primitive(\d+)$/;
+        const meshCacheInfo: MeshCacheInfo = {};
+        for (const nodeIndex of Object.keys(this.meshCache).map(Number)) {
+            const nodeMeshInfo: NodeMeshInfo = {
+                nodeIndex: -1,
+                nodeMeshes: [],
+                nodeParent: null,
+                nodeSkeleton: null,
+                nodeType: ""
+            }
+            for (let index = 0; index < this.meshCache[nodeIndex].length; index++) {
+                const primitive = this.meshCache[nodeIndex][index];
+                console.log(primitive.name);
+
+                const match = primitive.name.match(pattern);
+                if (match) {
+                    switch (match[1]) {
+                        case "Body":
+                            nodeMeshInfo.nodeType = "Body";
+                            meshCacheInfo.Body = nodeMeshInfo;
+                            break;
+                        case "Face":
+                            nodeMeshInfo.nodeType = "Face";
+                            meshCacheInfo.Face = nodeMeshInfo;
+                            break;
+                        case "Hair":
+                            nodeMeshInfo.nodeType = "Hair";
+                            meshCacheInfo.Hair = nodeMeshInfo;
+                            break;
+                    }
+
+                    // Push meshes
+                    nodeMeshInfo.nodeIndex = nodeIndex;
+                    nodeMeshInfo.nodeMeshes.push(primitive);
+                    // Get parent
+                    if (!nodeMeshInfo.nodeParent)
+                        nodeMeshInfo.nodeParent = primitive.parent;
+                    else
+                    if (primitive.parent?.name !== nodeMeshInfo.nodeParent.name)
+                        throw new Error("Mesh parent doesn't match!");
+                    // Get skeleton
+                    if (!nodeMeshInfo.nodeSkeleton)
+                        nodeMeshInfo.nodeSkeleton = primitive.skeleton;
+                    else
+                    if (primitive.skeleton !== nodeMeshInfo.nodeSkeleton)
+                        throw new Error("Mesh skeleton doesn't match!");
+                }
+            }
+        }
+
+        return meshCacheInfo;
+    }
+
+    private createImposter(
+        mesh: Mesh,
+        type: number,
+        options: PhysicsImpostorParameters,
+        ignoreParent: boolean = false,
+        reparent: boolean = true,
+        recursiveParent: boolean = true,
+        showImposter: boolean = true
+    ) {
+        console.log("createImposter");
+        let parent = mesh.parent;
+        while (recursiveParent && parent?.parent)
+            parent = parent.parent;
+        if (reparent) mesh.setParent(null);
+        // if (reparent) this.setParentWrapper(mesh, null);
+
+        mesh.physicsImpostor = new PhysicsImpostor(mesh, type,
+            {...options, ignoreParent: ignoreParent},
+            this.scene);
+        if (this.physicsDebug && showImposter)
+            try {
+                this.physicsViewer.showImpostor(mesh.physicsImpostor, mesh);
+            } catch (exc) {
+                console.warn(exc);
+            }
+        if (reparent) mesh.setParent(parent);
+        // if (reparent) this.setParentWrapper(mesh, parent);
+    }
+
+    private logEssenstial(mesh: Nullable<AbstractMesh> | Nullable<TransformNode>) {
+        if (mesh) {
+            console.log(mesh.name);
+            console.log(mesh.position);
+            console.log(mesh.rotation);
+            console.log(mesh.scaling);
+            console.log(mesh.getWorldMatrix())
+            console.log(mesh.rotationQuaternion)
+        }
     }
 }
